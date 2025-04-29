32abdedfec7dac74008e7e3f142794aeb7fcb777
DG <ehd666@kumoh.ac.kr>
2025-04-29 16:02:34 +0900

    carvery init

---
+++ src/pages/Main.svelte
@@ -0,0 +1,233 @@
+<header>
+  <div class="gnb">
+    <div class="logo-frame">
+      <div class="logo"><a href="/home">Carvery</a></div>
+    </div>
+    <div class="menu-frame">
+      <div class="menu"><div class="menu-text"><a href="/">날씨</a></div></div>
+      <div class="menu">
+        <div class="menu-text"><a href="/">세차장</a></div>
+      </div>
+      <div class="menu"><div class="menu-text"><a href="/">소식</a></div></div>
+      <div class="menu">
+        <div class="menu-text"><a href="/">세차용품</a></div>
+      </div>
+      <div class="menu"><div class="menu-text"><a href="/">지도</a></div></div>
+    </div>
+    <div class="search-bar-frame">
+      <form class="gnb-search-bar-frame" action="" method="get">
+        <object title="" type="image/svg+xml" data="/assets/search.svg"></object>
+        <input class="search-input" type="text" />
+      </form>
+    </div>
+  </div>
+</header>
+<div class="home">
+  <div class="weather-frame">
+    <div class="weather-Icon">
+      <object
+        title=""
+        class="w-Icon"
+        type="image/svg+xml"
+        data="/weathers/null-today.svg"
+      ></object>
+    </div>
+    <div class="weather-board">
+      <div class="today-frame">
+        <div class="recommend-frame">
+          <p class="ment">세차하기 좋은 날입니다.</p>
+          <p class="info">-0000년 00월 00일-</p>
+        </div>
+        <div class="divier"></div>
+        <div class="information-frame">
+          <p class="temp">0°</p>
+          <div class="location-frame">
+            <p class="info-text">사용자 위치</p>
+            <object title="" type="image/svg+xml" data="/assets/location.svg"
+            ></object>
+          </div>
+        </div>
+      </div>
+      <div class="weekly-frame">
+        <div class="weather-info">
+          <p class="temp">기온</p>
+          <object
+            title=""
+            type="image/svg+xml"
+            data="/weathers/weather_default.svg"
+          ></object>
+          <p class="temp">요일</p>
+        </div>
+        <div class="weather-info">
+          <p class="temp">기온</p>
+          <object
+            title=""
+            type="image/svg+xml"
+            data="/weathers/weather_default.svg"
+          ></object>
+          <p class="temp">요일</p>
+        </div>
+        <div class="weather-info">
+          <p class="temp">기온</p>
+          <object
+            title=""
+            type="image/svg+xml"
+            data="/weathers/weather_default.svg"
+          ></object>
+          <p class="temp">요일</p>
+        </div>
+        <div class="weather-info">
+          <p class="temp">기온</p>
+          <object
+            title=""
+            type="image/svg+xml"
+            data="/weathers/weather_default.svg"
+          ></object>
+          <p class="temp">요일</p>
+        </div>
+        <div class="weather-info">
+          <p class="temp">기온</p>
+          <object
+            title=""
+            type="image/svg+xml"
+            data="/weathers/weather_default.svg"
+          ></object>
+          <p class="temp">요일</p>
+        </div>
+        <div class="weather-info">
+          <p class="temp">기온</p>
+          <object
+            title=""
+            type="image/svg+xml"
+            data="/weathers/weather_default.svg"
+          ></object>
+          <p class="temp">요일</p>
+        </div>
+        <div class="weather-info">
+          <p class="temp">기온</p>
+          <object
+            title=""
+            type="image/svg+xml"
+            data="/weathers/weather_default.svg"
+          ></object>
+          <p class="temp">요일</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="one-line-card-frame">
+    <p class="title">세차장</p>
+    <div class="card-frame">
+      <object
+        title=""
+        class="nav-button"
+        type="image/svg+xml"
+        data="/assets/left_button.svg"
+      ></object>
+      <div class="card-list-frame">
+        <div class="card-list">
+          <div class="card">
+            <div class="info-head">
+              <p class="name">업체명</p>
+              <div class="divier"></div>
+              <div class="location-frame">
+                <object
+                  title=""
+                  class="location_small"
+                  type="image/svg+xml"
+                  data="/assets/location_small.svg"
+                ></object>
+                <p class="distance">000m</p>
+              </div>
+            </div>
+            <div class="image"></div>
+            <div class="content-frame">
+              <p class="title">주소</p>
+              <p class="content">00시 00구 00로 000-0</p>
+            </div>
+            <div class="content-frame">
+              <p class="title">시설 정보</p>
+              <div class="info-list">
+                <object
+                  title=""
+                  class="info-icon"
+                  type="image/svg+xml"
+                  data="/facilities/hand.svg"
+                ></object>
+              </div>
+            </div>
+            <div class="content-frame">
+              <p class="title">전화번호</p>
+              <p class="content">000-000-0000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <object
+        title=""
+        class="nav-button"
+        type="image/svg+xml"
+        data="/assets/right_button.svg"
+      ></object>
+    </div>
+  </div>
+  <div class="one-line-card-frame">
+    <p class="title">정비소</p>
+    <div class="card-frame">
+      <object
+        title=""
+        class="nav-button"
+        type="image/svg+xml"
+        data="/assets/left_button.svg"
+      ></object>
+      <div class="card-list-frame">
+        <div class="card-list">
+          <div class="card">
+            <div class="info-head">
+              <p class="name">업체명</p>
+              <div class="divier"></div>
+              <div class="location-frame">
+                <object
+                  title=""
+                  class="location_small"
+                  type="image/svg+xml"
+                  data="/assets/location_small.svg"
+                ></object>
+                <p class="distance">000m</p>
+              </div>
+            </div>
+            <div class="image"></div>
+            <div class="content-frame">
+              <p class="title">주소</p>
+              <p class="content">00시 00구 00로 000-0</p>
+            </div>
+            <div class="content-frame">
+              <p class="title">시설 정보</p>
+              <div class="info-list">
+                <object
+                  title=""
+                  class="info-icon"
+                  type="image/svg+xml"
+                  data="/assets/facilities/hand.svg"
+                ></object>
+              </div>
+            </div>
+            <div class="content-frame">
+              <p class="title">전화번호</p>
+              <p class="content">000-000-0000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <object
+        title=""
+        class="nav-button"
+        type="image/svg+xml"
+        data="/assets/right_button.svg"
+      ></object>
+    </div>
+  </div>
+</div>
